cd9c54b9beca21532fea977ec1fbed1bcfe3c1b2
Nilo <ndroock1@gmail.com>
2018-07-24 18:43:31 +0200

    Upto video 3.17

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-let name = prompt("What is your name?");
+// Stuff
+let name = "Nilo de Roock"; // prompt("What is your name?");
 let message = ` Ten Onwelle !`
 document.getElementById("welcome").innerHTML='Welcome ' + name + message;
 
-let cart = ['A', 'B', 'C'];
-// console.table(cart);
-// console.log(typeof cart);
-
-const learning = "Learning JavaScript is Great!";
 let output;
+output = new Date().toString();
+let msg = document.querySelector("#message");
+msg.innerHTML = output;
+
+
+// Strings
+const learning = "Learning JavaScript is Great!";
 output = learning.length;
 output = learning.indexOf("JavaScript");
 output = learning.substr(9,10);
 output = learning.split(" ");
-
-output = new Date().toString();
-
 console.log(output);
-let msg = document.querySelector("#message");
-msg.innerHTML = output;
+
+// Arrays
+let cart = ['A', 'B', 'C'];
+let arr = new Array(1,2,3,4,5);
+arr.push(10);
+arr.unshift(-123);
+arr.splice(4,"",100);
+console.log(arr);
+arr.pop();
+arr.shift();
+arr.splice(3,1);
+console.log(arr);
