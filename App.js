9c40a5d1b0b18d775e2408d38b718648e105e373
Nilo <ndroock1@gmail.com>
2018-07-25 18:10:35 +0200

    Upto video 3.24

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ output = learning.substr(9,10);
 output = learning.split(" ");
 console.log(output);
 
+
 // Arrays
 let cart = ['A', 'B', 'C'];
 let arr = new Array(1,2,3,4,5);
@@ -27,4 +28,69 @@ console.log(arr);
 arr.pop();
 arr.shift();
 arr.splice(3,1);
-console.log(arr);
+let arr1 = new Array(11,12,13,14,15);
+let arr2 = arr1.concat(arr);
+// Order from lower to greater
+arr2.sort(function(num1, num2) {
+    return num1 - num2;
+    }
+);
+console.log(arr2);
+
+//Objects
+const person = {
+    name: 'Juan',
+    lastName: 'De la torre',
+    job: 'Web Developer',
+    email: 'mail@mail.com',
+    age : 20,
+    favoriteMusic: ['Trance', 'Rock', 'Grunge'],
+    living: {
+        city: 'Guadalajara',
+        country: 'Mexico'
+    },
+    birthYear: function() {
+        return new Date().getFullYear() - this.age;
+    }
+}
+console.log("Geboortejaar = " + person['birthYear']);
+console.log("Geboortejaar = " + person['birthYear']());
+console.log(person.birthYear());
+
+//Functions
+const musicPlayer = {
+    play: function(id) {
+        console.log(`Playing song with the ID: ${id}`);
+    },
+    pause: function() {
+        console.log('paused....');
+    }
+}
+musicPlayer.play(30);
+musicPlayer.pause();
+//Immediately-Invoked Function Expression
+(function() {
+    console.log('IIFES!!');
+})();
+
+//Dates
+//Dates in JavaScript are objects so you have to create a new date.
+const today = new Date();
+
+console.log(today);
+// Date MM-DAY-YEAR
+let birthday = new Date('17-2-1959');
+// Another way
+birthday = new Date('17 February 1959');
+console.log(birthday);
+//console.log(output);
+// output = today.getMonth();
+// output = today.getDate();
+// output = today.getDay();
+// output = today.getFullYear();
+output = today.getMinutes();
+// output = today.getHours();
+// output = today.getTime();
+// output = today.getFullYear();
+// output = today.setFullYear(2018);
+console.log(output);
